b6fb3dba82bf84655200ee84c78cd327e897830a
Maneesh Mehta <maneesh.mehta@synapse.com>
2018-09-14 17:02:16 +0530

    remove static from interface.

--- enc/Enc.php
+++ enc/Enc.php
@@ -2,8 +2,8 @@
 namespace enc;
 
 interface Enc{
-    public static function encrypt($input);
+    public function encrypt($input);
     
-    public static function decrypt($input);
+    public function decrypt($input);
 }
 ?>
